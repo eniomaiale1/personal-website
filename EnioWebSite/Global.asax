3f273d1fe2f4e426f705a60f4e253a82e724238b
Enio Maiale <emaiale@phunware.com>
2016-12-27 15:07:34 -0500

    Initial commit

---
+++ EnioWebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnioWebSite.MvcApplication" Language="C#" %>
---
+++ EnioWebSite/Models/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnioWebSite.Models
+{
+    public class ContactModel
+    {
+        [Required(ErrorMessage ="Contact name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email address is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Message can not be emty.")]
+        public string Message { get; set; }
+    }
+}
